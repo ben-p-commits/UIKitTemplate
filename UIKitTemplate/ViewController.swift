849af0e2e558e80ef27dcf6b8f0e8c51eeaad6a1
Ben Palmer <benj.c.palmer@gmail.com>
2025-02-16 08:10:04 -0500

    Initial Commit

---
+++ UIKitTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTemplate
+//
+//  Created by Ben Palmer on 2/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
